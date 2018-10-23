48e16e61067f374cb3f64bdbf325ffa786e626bd
mazek <a>
2018-10-23 17:05:21 +0200

    Init Commit

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ export class App extends Component{
     render() {
         return (
             <View>
-                {"Hello World"}
+                {"Hello World Init"}
             </View>
     );
     }
